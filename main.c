9205f7fec16a0e874ae0398815c1fe7eeca6f3f8
Harish <harishvigneshcoc@gmail.com>
2026-04-12 16:17:45 +0530

    updated otp part in generate otp func..

--- main.c
+++ main.c
@@ -1,27 +1,8 @@
 #include <lpc21xx.h>
 #include "main_operation.h"
 
-void OTP_GENERATE_ISR(void) __irq
-{
-	
-}
-
 int main()
-{	
-	
-	 /* ---- EINT1 CONFIGURATION ---- */
-		PINSEL0 |= 0x20000000;
-		VICIntSelect = 0;
-															// Allocate IRQ SLOT-0
-		VICVectCntl0 = (0x20)|15;
-		VICVectAddr0 = (unsigned long)OTP_GENERATE_ISR;
-		
-		EXTMODE  = 0x00;
-		EXTPOLAR = 0x00;
-	
-		VICIntEnable = (1<<15);
-	 /* ----------------------------- */
-		
+{		
 	INIT_FUNC();   	
   STORE_PASS("2134");
 	TOPIC();
--- main_operation.h
+++ main_operation.h
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include "lcd.h"
 #include "i2c_operation.h"
 #include "keypad.h"
@@ -6,6 +7,7 @@
 #define SW 14
 
 i32 attempts = 0;
+i32 otp = 0;
 u8 p[10];						// PASSWORD STORAGE
 u8 e[10];     			// ENTERED PASSWORD
 
@@ -17,7 +19,6 @@ void ENTER_PASS(void);
 void COMPARE_PASS(void);
 void ENTER_OTP(u8*);
 void GENERATE_OTP(void);
-
 
 /* ---- INITIALIZATIONS ----  */
 
@@ -54,7 +55,7 @@ void ENTER_PASS()
 	for(i=0,addr=0x99;i<4;i++)
 	{
 		e[i] = keypress();
-		lcd_cmd(addr+=2);lcd_write(e[i]);
+		lcd_cmd(addr+=2);lcd_write('*');
 	}
 	e[i] = '\0';
 }
@@ -64,15 +65,16 @@ void ENTER_PASS()
 void TOPIC()
 {
 	i32 i;
-	lcd_cmd(0x80); lcd_str("MULTI LEVEL SECURITY");
-	lcd_cmd(0xC0); lcd_str("   ACCESS CONTROL   ");
-	lcd_cmd(0x94); lcd_str("       SYSTEM       ");
-	lcd_cmd(0xD4);
-				for(i=0;i<20;i++)
-				{
-					lcd_write('~');
-					delay_ms(200);
-				}
+	u8 st,end;
+	
+	lcd_cmd(0xC0); lcd_str("MULTI LEVEL SECURITY");
+	lcd_cmd(0x94); lcd_str("ACCESS CONTROL SYSTM");
+	for(i=0, st=0x80, end=0xD4; i<20; i++)
+	{
+		lcd_cmd(st++);lcd_write('~');
+		lcd_cmd(end++);lcd_write('~');
+		delay_ms(200);
+	}
 }
 
 /* ----- COMPARE PASSWORD ----- */
@@ -81,77 +83,95 @@ void COMPARE_PASS()
 {
 	u8 otp[5];
 	if(!strcmp((char *)p,(char *)e))
-			{
-				attempts = 0;
-				lcd_cmd(0x01);
-				lcd_cmd(0x80);lcd_str("  >>>  SUCCESS  <<< ");
-				lcd_cmd(0xC0);lcd_str("     PRESS SWITCH   ");
-				lcd_cmd(0x94);lcd_str("   To Generate otp  ");
-				lcd_cmd(0xD4);lcd_str(" ------------------ ");
-				ENTER_OTP(otp);
-			}
-			else
-			{
-				attempts++;
-				
-				lcd_cmd(0x01);
-				lcd_cmd(0x80);lcd_str("====================");
-				lcd_cmd(0xC0);lcd_str("      IN-VALID      ");
-				lcd_cmd(0x94);lcd_str("      PASSWORD      ");
-				lcd_cmd(0xD4);lcd_str("====================");
-				
-				delay_ms(1200);lcd_cmd(0x01);
-				
-				lcd_cmd(0xC0);lcd_str("   ATTEMPTS LEFT:   ");
-				lcd_cmd(0x9D);lcd_write('0');lcd_write('0' + (3 - attempts));  
-
-        delay_ms(1200);
-
-        // LOCK CONDITION
-        if(attempts >= 3)
-        {
-            lcd_cmd(0x01);
-            lcd_cmd(0x80);lcd_str(" ****************** ");
-            lcd_cmd(0xC0);lcd_str("       SYSTEM       ");
-            lcd_cmd(0x94);lcd_str("       LOCKED       ");
-            lcd_cmd(0xD4);lcd_str(" ****************** ");
-
-            while(1);   // permanent lock
-        }
-			}
-}
-
-void ENTER_OTP(u8 *p)
-{
-	i32 i,addr;
-	
-	GENERATE_OTP();
-	
-	lcd_cmd(0x01);
-  lcd_cmd(0x80);lcd_str("********************");
-  lcd_cmd(0xC0);lcd_str("     ENTER OTP      ");
-  lcd_cmd(0xD4);lcd_str("********************");
-	for(i=0,addr=0x99;i<4;i++)
 	{
-		p[i] = keypress();
-		lcd_cmd(addr+=2);lcd_write(p[i]);
+		attempts = 0;
+		lcd_cmd(0x01);
+		lcd_cmd(0x80);lcd_str("  (^_^) ACCESS OK!  ");
+		lcd_cmd(0xC0);lcd_str(" ------------------ ");
+		lcd_cmd(0x94);lcd_str("   PRESS ~ BUTTON   ");
+		lcd_cmd(0xD4);lcd_str("    GENERATE OTP    ");
+		//delay_ms(2000);
+		ENTER_OTP(otp);
 	}
-	p[i] = '\0';
-	
-	
-	lcd_cmd(0x01);
-  lcd_cmd(0x80);lcd_str(" ****************** ");
-  lcd_cmd(0xC0);lcd_str("   SYSTEM STATUS    ");
-  lcd_cmd(0x94);lcd_str("      UNLOCKED      ");
-  lcd_cmd(0xD4);lcd_str(" ****************** ");
-	while(1);
-	
+	else
+	{
+		attempts++;
+		
+		lcd_cmd(0x01);
+		lcd_cmd(0x80);lcd_str("====================");
+		lcd_cmd(0xC0);lcd_str("      IN-VALID      ");
+		lcd_cmd(0x94);lcd_str("      PASSWORD      ");
+		lcd_cmd(0xD4);lcd_str("====================");
+		
+		delay_ms(1200);lcd_cmd(0x01);
+		
+		lcd_cmd(0xC0);lcd_str("   ATTEMPTS LEFT:   ");
+		lcd_cmd(0x9D);lcd_write('0');lcd_write('0' + (3 - attempts));  
+
+		delay_ms(1200);
+
+		// LOCK CONDITION
+		if(attempts >= 3)
+		{
+				lcd_cmd(0x01);
+				lcd_cmd(0x80);lcd_str(" ****************** ");
+				lcd_cmd(0xC0);lcd_str("       SYSTEM       ");
+				lcd_cmd(0x94);lcd_str("       LOCKED       ");
+				lcd_cmd(0xD4);lcd_str(" ****************** ");
+
+				while(1);   // permanent lock
+		}
+	}
 }
 
 void GENERATE_OTP()
 {
-	T0PR  = 10000 - 1;
-	T0TCR = 0x01;
-	while(!((IOPIN0>>SW)&1));
+	T1PR  = 15000 - 1;
+	T1TCR = 0x01;
+
+	while(((IOPIN0>>SW)&1)==1);  
+	delay_ms(150);
+
+	otp = (T1TC % 8000) + 2000;  
+
+	while(((IOPIN0>>SW)&1)==0);   
+
+	T1TCR = 0x03;
+	T1TCR = 0x00;
+}
+
+void ENTER_OTP(u8 *p)
+{
+	i32 i, addr;
+
+	GENERATE_OTP();
+	// Here comes UART
+
+	lcd_cmd(0x01);
+	lcd_cmd(0x80);lcd_str(" ------------------ ");
+	lcd_cmd(0xC0);lcd_str("     ENTER OTP      ");
+	lcd_cmd(0xD4);lcd_str(" ------------------ ");
+	lcd_cmd(0x94);
+
+	for(i=0,addr=0x98;i<4;i++)
+	{
+		p[i] = keypress();
+		lcd_cmd(addr+=2);
+		lcd_write(p[i]);   
+	}
+	p[i] = '\0';
+
+	if((p[0]-48)*1000 + (p[1]-48)*100 + (p[2]-48)*10 + (p[3]-48) == otp)
+	{
+		lcd_cmd(0x01);
+		lcd_str("ACCESS GRANTED");
+	}
+	else
+	{
+		lcd_cmd(0x01);
+		lcd_str("WRONG OTP");
+	}
+
+	while(1);
 }
 
